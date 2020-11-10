2e6d7133ab080dd942ddf6c610562bf84df1b45d
Ben D <33376747+BenD@users.noreply.github.com>
2020-11-10 22:09:20 +0000

    Initial commit

---
+++ static/PubQuizJS.js
@@ -0,0 +1,9 @@
+$(".about-bar").click(function() {
+  $expandable = $(this);
+  $content = $expandable.next();
+  $content.slideToggle(500, function() {
+    $expandable.text(function() {
+      return $content.is(":visible") ? "Close" : "About";
+    });
+  });
+});
