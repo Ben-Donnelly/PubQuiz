a28ed563e590bf1636e133d13e3e30db018e7fb5
Ben D <33376747+BenD@users.noreply.github.com>
2020-11-22 20:47:15 +0000

    Several new route end points, some hacky code that needs to be fixed up

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from firebase import firebase
 from wtforms import Form, IntegerField, DateField, PasswordField, StringField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+from io import BytesIO
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import base64
+
 app = Flask(__name__)
 firebase = firebase.FirebaseApplication("https://pubquiztracker.firebaseio.com/", None)
 
@@ -28,13 +34,14 @@ def arts():
         res = score_result[k]
         scores_l.append(res['Scores'])
 
-    print(scores_l)
+    # print(scores_l)
     values = list(k['Scores'] for k in score_result.values())
     values = (max(map(len, values)))
     user_d = {k: v['Name'] for k, v in enumerate(user_result.values())}
-    print(f"num_users: {user_d}\nnum_rows: {values}\nscores_l: {scores_l}")
-
-    return render_template("leaderboard.html", num_users=user_d, num_rows=values, scores=scores_l)
+    # print(f"num_users: {user_d}\nnum_rows: {values}\nscores_l: {scores_l}")
+    tot_scores = [sum(i) for i in scores_l]
+    # print(tot_scores)
+    return render_template("leaderboard.html", num_users=user_d, num_rows=values, scores=scores_l, tot_scores=tot_scores)
 
 
 class register_form(Form):
@@ -136,7 +143,7 @@ def logout():
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", curr_user=session['username'])
 
 
 class ScoreForm(Form):
@@ -174,7 +181,84 @@ def add_score():
         return redirect(url_for("dashboard"))
     return render_template("add-score.html", form=form)
 
+@app.route("/update/scores")
+@is_logged_in
+def update_scores():
+    score_result = firebase.get(f"/pubquiztracker/Scores/{session['username']}", "")
 
+    scores_l = []
+    for k in score_result:
+        scores_l = score_result[k]
+
+    print(scores_l)
+    values = len(scores_l)
+    print(f"user: {session['username']}\nnum_rows: {values}\nscores_l: {scores_l}")
+
+    return render_template("updateScores.html", curr_user=session['username'], num_rows=values, scores=scores_l)
+    # return render_template("updateScores.html", curr_user=session['username'], num=getScoresForUpdate)
+
+@app.route(f"/<string:curr_user>/stats")
+@is_logged_in
+def stats(curr_user):
+    score_result = firebase.get(f"/pubquiztracker/Scores/{session['username']}", "")
+
+    scores_l = []
+    for k in score_result:
+        scores_l = score_result[k]
+
+    img = BytesIO()
+    len_scores = len(scores_l)
+    x = [f"Week: {i+1}" for i in range(len_scores)]
+
+    average = sum(scores_l) / len_scores
+    if average.is_integer():
+        average = int(average)
+    best = max(scores_l)
+    worst = min(scores_l)
+
+    score_stats = [average, best, worst]
+
+    plt.plot(x, scores_l, '--ko')
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('curUserStats.html', plot_url=plot_url, curr_user=session['username'], score_stats=score_stats)
+
+@app.route(f"/overall/stats")
+@is_logged_in
+def overall_stats():
+    score_result = firebase.get(f"/pubquiztracker/Scores", "")
+    img = BytesIO()
+
+    # Hacky (works so good enough for now), ****FIX LATER****
+    x = list(score_result.keys())
+    y = list(sum(i['Scores']) for i in score_result.values())
+
+    print(x)
+    print(y)
+
+    data = dict(zip(x, y))
+
+    labels = list(data.keys())
+    sizes = list(data.values())
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('overallStats.html', curr_user=session['username'], plot_url=plot_url)
+
+# with app.test_request_context():
+#     print(url_for('overall_stats'))
 if __name__ == "__main__":
     app.secret_key = '>\xcdN\x9f\xcc\x0f<\xec\xb0x\x8em~\xc6\x16\xae~?&\xc2\x81\xa9\xa1&'
     app.run(debug=True)
