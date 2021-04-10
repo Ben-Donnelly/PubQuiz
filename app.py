d98a9ff38b7ea9fe08999c56bbca6322a697de79
Ben D <33376747+BenD@users.noreply.github.com>
2021-04-10 21:48:18 +0100

    Integrated Chart.JS, fixed some bugs, code still needs cleaning
    Integrated Chart.JS for better looking graphs (instead of the previous images).
    
    Squished a bug where for the line graph it would only take the first persons max score for the line graph and
    if this wasnt the largest number (e.g person 0 has 13 quizzes done, but person 1 has 14) it would crash.
    **NOTE** This is no longer needed as the chart is now rendered using Chart.js, the functionality will be
    removed in the next commit (i.e. code cleanup)

--- app.py
+++ app.py
@@ -14,349 +14,332 @@ from statistics import mean
 import numpy as np
 matplotlib.use('Agg')
 
-# AWS WSGI looks for application by default
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '>\xcdN\x9f\xcc\x0f<\xec\xb0x\x8em~\xc6\x16\xae~?&\xc2\x81\xa9\xa1&'
 
-cache = Cache(app)
+# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 firebase = firebase.FirebaseApplication("https://pubquiztracker.firebaseio.com/", None)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+	return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+	return render_template('about.html')
 
 
 def get_required_data(parent_dir="Scores", user_name="", endpoint=""):
-    return firebase.get(f"/pubquiztracker/{parent_dir}/{user_name}", endpoint)
+	return firebase.get(f"/pubquiztracker/{parent_dir}/{user_name}", endpoint)
 
 
 @app.route("/leaderboard")
-@cache.cached()
+# @cache.cached()
 def leaderboard():
-    score_result = get_required_data()
+	score_result = get_required_data()
 
-    scores_l = []
-    m_rows = 0
+	scores_l = []
+	m_rows = 0
 
-    for k, v in score_result.items():
-        # Makes list of lists of each users scores
-        individual_user_scores = score_result[k]
-        scores_l.append(individual_user_scores['Scores'])
+	for k, v in score_result.items():
+		# Makes list of lists of each users scores
+		individual_user_scores = score_result[k]
+		scores_l.append(individual_user_scores['Scores'])
 
-        # If 1 person has done 10 quizzes but another only 1 | 2 | 3 | ... | 9
-        # We still need 10 rows
-        m_rows = max(m_rows, v['num_entries'])
+		# If 1 person has done 10 quizzes but another only 1 | 2 | 3 | ... | 9
+		# We still need 10 rows
+		m_rows = max(m_rows, v['num_entries'])
 
-    # Use of dictionary for better integrity
-    user_d = {k: v for k, v in enumerate(list(score_result.keys()))}
+	# Use of dictionary for better integrity
+	user_d = {k: v for k, v in enumerate(list(score_result.keys()))}
 
-    # For total scores row
-    t_scores = [sum(i) for i in scores_l]
+	# For total scores row
+	t_scores = [sum(i) for i in scores_l]
 
-    return render_template("leaderboard.html", num_users=user_d, num_rows=m_rows, scores=scores_l, tot_scores=t_scores)
+	return render_template("leaderboard.html", num_users=user_d, num_rows=m_rows, scores=scores_l, tot_scores=t_scores)
 
 
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=2, max=25)])
-    username = StringField('Username', [validators.Length(min=3, max=25)])
-    email = EmailField('Email', [validators.Length(min=6, max=40)])
-    password = PasswordField('Password', [
-        validators.data_required(),
-        validators.EqualTo('confirm', message='Passwords do not match.'),
-        validators.length(min=4)
-    ])
-    confirm = PasswordField('Confirm password')
+	name = StringField('Name', [validators.Length(min=2, max=25)])
+	username = StringField('Username', [validators.Length(min=3, max=25)])
+	email = EmailField('Email', [validators.Length(min=6, max=40)])
+	password = PasswordField('Password', [
+		validators.data_required(),
+		validators.EqualTo('confirm', message='Passwords do not match.'),
+		validators.length(min=4)
+	])
+	confirm = PasswordField('Confirm password')
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    flash("This application was made for a small number of people and so no new users are able to register at this "
-          "point. Please contact the site owner if you have any questions. This page is left in for "
-          "demonstration purposes only", 'warning')
-    form = RegisterForm(request.form)
+	flash("This application was made for a small number of people and so no new users are able to register at this "
+		  "point. Please contact the site owner if you have any questions. This page is left in for "
+		  "demonstration purposes only", 'warning')
+	form = RegisterForm(request.form)
 
-    return render_template('register.html', form=form)
+	return render_template('register.html', form=form)
 
 
 #  Login
 def check_user():
-    email = request.form['email']
-    result = get_required_data(parent_dir="Users")
-    for k, v in result.items():
-        pot_user = v["Email"]
-        if pot_user == email:
-            return v['Username'], v['Password']
+	email = request.form['email']
+	result = get_required_data(parent_dir="Users")
+	for k, v in result.items():
+		pot_user = v["Email"]
+		if pot_user == email:
+			return v['Username'], v['Password']
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        password_candidate = request.form['password']
-        creds = check_user()
-        if creds:
-            username = creds[0]
-            check_pass = creds[1]
+	if request.method == "POST":
+		password_candidate = request.form['password']
+		creds = check_user()
+		if creds:
+			username = creds[0]
+			check_pass = creds[1]
 
-            if sha256_crypt.verify(password_candidate, check_pass):
-                #  Passed
-                session['logged_in'] = True
-                session['username'] = username
+			if sha256_crypt.verify(password_candidate, check_pass):
+				#  Passed
+				session['logged_in'] = True
+				session['username'] = username
 
-                flash("You are now logged in!", 'success')
-                return redirect(url_for('dashboard'))
-            else:
-                error_msg = "Invalid login"
-                return render_template("login.html", error=error_msg)
+				flash("You are now logged in!", 'success')
+				return redirect(url_for('dashboard'))
+			else:
+				error_msg = "Invalid login"
+				return render_template("login.html", error=error_msg)
 
-        else:
-            error_msg = "Email or password does not match"
-            return render_template("login.html", error=error_msg)
+		else:
+			error_msg = "Email or password does not match"
+			return render_template("login.html", error=error_msg)
 
-    return render_template("login.html")
+	return render_template("login.html")
 
 
 # Check user logged in
 def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash("Unauthorised, please login", 'danger')
-            return redirect(url_for('login'))
-    return wrap
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash("Unauthorised, please login", 'danger')
+			return redirect(url_for('login'))
+	return wrap
 
 
 @app.route("/logout")
 @is_logged_in
 def logout():
-    curr_user = session['username']
-    session.clear()
-    flash(f"You have been successfully logged out!\nBye {curr_user}! 😥", "success")
-    return redirect(url_for("login"))
+	curr_user = session['username']
+	session.clear()
+	flash(f"You have been successfully logged out!\nBye {curr_user}! 😥", "success")
+	return redirect(url_for("login"))
 
 
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template("dashboard.html", curr_user=session['username'])
+	return render_template("dashboard.html", curr_user=session['username'])
 
 
 def get_scores_for_update(new_score, u_name):
-    try:
-        # Gets all scores list for current user
-        u_list = get_required_data(user_name=u_name)['Scores']
-        # Adds new score
-        u_list.append(new_score)
-    except TypeError:
-        flash("Something went wrong, tell Ben and give him the values you put in for score and date!", "danger")
-        return redirect(url_for("add_score"))
+	try:
+		# Gets all scores list for current user
+		u_list = get_required_data(user_name=u_name)['Scores']
+		# Adds new score
+		u_list.append(new_score)
+	except TypeError:
+		flash("Something went wrong, tell Ben and give him the values you put in for score and date!", "danger")
+		return redirect(url_for("add_score"))
 
-    return u_list
+	return u_list
 
 
 class ScoreForm(Form):
-    score = IntegerField("Score", [validators.data_required(message="You need to enter your score as a number")])
-    date = DateField("Date")
+	score = IntegerField("Score", [validators.data_required(message="You need to enter your score as a number")])
+	date = DateField("Date")
 
 
 def update_user_average(scores_l):
-    # Gets number of quizzes completed and computes the new average
+	# Gets number of quizzes completed and computes the new average
 
-    # The new score has already been added, saves call to db
-    curr_num_of_scores = len(scores_l)
-    new_average = round(mean(scores_l), 2)
+	# The new score has already been added, saves call to db
+	curr_num_of_scores = len(scores_l)
+	new_average = round(mean(scores_l), 2)
 
-    return [new_average, curr_num_of_scores]
+	return [new_average, curr_num_of_scores]
 
 
 @app.route("/add/score", methods=['GET', 'POST'])
 @is_logged_in
 def add_score():
-    form = ScoreForm(request.form)
-    # Clear cache so that the leaderboard will be updated
-    cache.clear()
+	form = ScoreForm(request.form)
+	# Clear cache so that the leaderboard will be updated
+	# cache.clear()
 
-    if request.method == "POST" and form.validate():
+	if request.method == "POST" and form.validate():
 
-        score = form.score.data
-        date = form.date.data
+		score = form.score.data
+		date = form.date.data
 
-        # If date in future, retry
-        if date > sys_date.today():
-            flash("Date cannot be in the future!", "danger")
-            return redirect(url_for("add_score"))
+		# If date in future, retry
+		if date > sys_date.today():
+			flash("Date cannot be in the future!", "danger")
+			return redirect(url_for("add_score"))
 
-        # Don't need else as redirects if > today's date
-        new_score = get_scores_for_update(score, session["username"])
+		# Don't need else as redirects if > today's date
+		new_score = get_scores_for_update(score, session["username"])
 
-        # below gives: list: [new_average, new_num_entries]
-        new_entries = update_user_average(new_score)
-        data = {
-            "Average": new_entries[0],
-            "Scores": new_score,
-            "num_entries": new_entries[1]
-        }
+		# below gives: list: [new_average, new_num_entries]
+		new_entries = update_user_average(new_score)
+		data = {
+			"Average": new_entries[0],
+			"Scores": new_score,
+			"num_entries": new_entries[1]
+		}
 
-        # Update the scores
-        firebase.put("/pubquiztracker/Scores", session["username"], data)
-        flash("Score entered!", "success")
+		# Update the scores
+		firebase.put("/pubquiztracker/Scores", session["username"], data)
+		flash("Score entered!", "success")
 
-        return redirect(url_for("dashboard"))
-    return render_template("add-score.html", form=form)
+		return redirect(url_for("dashboard"))
+	return render_template("add-score.html", form=form)
 
 
 def all_average():
-    score_result = get_required_data()
-    average_lis = [v['Average'] for v in score_result.values()]
-    return average_lis
+	score_result = get_required_data()
+	average_lis = [v['Average'] for v in score_result.values()]
+	return average_lis
 
 
 @app.route(f"/<string:curr_user>_stats")
-@cache.cached()
+# @cache.cached()
 @is_logged_in
 def stats(curr_user):
-    fig, ax = plt.subplots()
+	fig, ax = plt.subplots()
 
-    score_result = get_required_data(user_name=curr_user)
+	score_result = get_required_data(user_name=curr_user)
 
-    scores_l = score_result['Scores']
+	scores_l = score_result['Scores']
 
-    img = BytesIO()
-    len_scores = score_result['num_entries']
-    x = [f"{i + 1}" for i in range(len_scores)]
+	img = BytesIO()
+	len_scores = score_result['num_entries']
+	x = [f"{i + 1}" for i in range(len_scores)]
 
-    average = round(mean(scores_l), 2)
+	average = round(mean(scores_l), 2)
 
-    # Don't want to return e.g 20.0, but do want to return e.g 20.5
-    # So if the value is x.0, then just cast to int
-    if isinstance(average, int):
-        average = int(average)
+	# Don't want to return e.g 20.0, but do want to return e.g 20.5
+	# So if the value is x.0, then just cast to int
+	if isinstance(average, int):
+		average = int(average)
 
-    best = max(scores_l)
-    worst = min(scores_l)
+	best = max(scores_l)
+	worst = min(scores_l)
 
-    s_stats = [average, best, worst]
+	s_stats = [average, best, worst]
 
-    plt.plot(x, scores_l, '--ko', label="Your scores")
-    plt.hlines(y=average, xmin=0, xmax=len(scores_l)-1, label="Average")
-    ax.set_xlabel('Week No.')
+	plt.plot(x, scores_l, '--ko', label="Your scores")
+	plt.hlines(y=average, xmin=0, xmax=len(scores_l)-1, label="Average")
+	ax.set_xlabel('Week No.')
 
-    plt.legend()
-    plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)
-    plot_url = b64encode(img.getvalue()).decode('utf8')
+	plt.legend()
+	plt.savefig(img, format='png')
+	plt.close()
+	img.seek(0)
+	plot_url = b64encode(img.getvalue()).decode('utf8')
 
-    return render_template('curUserStats.html', plot_url=plot_url, curr_user=curr_user, score_stats=s_stats)
+	return render_template('curUserStats.html', plot_url=plot_url, curr_user=curr_user, score_stats=s_stats)
+
 
 def pie_chart(graph_usernames, custom_colours, graph_scores):
-    pie_img = BytesIO()
 
-    fig1, ax1 = plt.subplots()
+	pie_img = BytesIO()
 
-    # pie chart
-    ax1.pie(graph_scores, labels=graph_usernames, startangle=90, colors=custom_colours)
-    # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax1.axis('equal')
+	fig1, ax1 = plt.subplots()
 
-    plt.savefig(pie_img, format='png')
-    plt.close()
-    pie_img.seek(0)
-    pie_url = b64encode(pie_img.getvalue()).decode('utf8')
+	# pie chart
+	ax1.pie(graph_scores, labels=graph_usernames, startangle=90, colors=custom_colours)
+	# Equal aspect ratio ensures that pie is drawn as a circle.
+	ax1.axis('equal')
 
-    return pie_url
+	plt.savefig(pie_img, format='png')
+	plt.close()
+	pie_img.seek(0)
+	pie_url = b64encode(pie_img.getvalue()).decode('utf8')
+
+	return pie_url
+
 
 def barh_chart(graph_usernames, custom_colours, graph_scores):
-    barh_img = BytesIO()
+	barh_img = BytesIO()
 
-    # Horizontal bar chart
-    plt.barh(graph_usernames, graph_scores, color=custom_colours)
-    plt.savefig(barh_img, format='png')
-    plt.close()
-    barh_img.seek(0)
+	# Horizontal bar chart
+	plt.barh(graph_usernames, graph_scores, color=custom_colours)
+	plt.savefig(barh_img, format='png')
+	plt.close()
+	barh_img.seek(0)
 
-    return b64encode(barh_img.getvalue()).decode('utf8')
+	return b64encode(barh_img.getvalue()).decode('utf8')
 
-def barv_chart(graph_usernames, score_result, custom_colours):
-    barv_img = BytesIO()
 
-    labels = list(f"{i} ({score_result[i]['Average']})" for i in graph_usernames)
+def barv_chart(graph_usernames, score_result):
 
-    # Gets the average score for each user
-    all_avg_lis = all_average()
-    avg_of_avg = mean(all_avg_lis)
+	# Name plus average score
+	labels = list(f"{i} ({score_result[i]['Average']})" for i in graph_usernames)
 
-    # Vertical bar chart
-    plt.bar(labels, all_avg_lis, color=custom_colours)
+	# Gets the average score for each user
+	all_avg_lis = all_average()
+	avg_of_avg = round(mean(all_avg_lis), 2)
 
-    # The average of averages line
-    plt.hlines(y=avg_of_avg, xmin=0, xmax=len(all_avg_lis)-1, label="Average of averages", colors='black')
-    plt.title("Average scores")
-    plt.legend()
-
-    # Sets y-axis to go from 0 up to the maximun score + 1 so that there is an upper limit on the graph
-
-    plt.yticks(np.arange(0, max(all_avg_lis)+2, 1))
-    plt.savefig(barv_img, format='png')
-    plt.close()
-    barv_img.seek(0)
-    return b64encode(barv_img.getvalue()).decode('utf8')
+	return [labels, all_avg_lis, avg_of_avg]
 
 def overall_avgs_chart(graph_usernames, score_result, custom_colours):
-    fig, ax = plt.subplots()
+	x = 0
+	dict_of_each_players_scores = {}
 
-    img = BytesIO()
-    len_scores = score_result[next(iter(score_result))]['num_entries']
+	for i in graph_usernames:
+		# Needed for the x axis of graph
+		x = max(x, score_result[i]['num_entries'])
 
-    x = [f"{i + 1}" for i in range(len_scores)]
-    print(x)
-    # min_weeks =
-    num_of_weeks = [score_result[i]['Scores'] for i in graph_usernames]
+		dict_of_each_players_scores[i] = score_result[i]['Scores']
 
+	x = [f"{i + 1}" for i in range(x)]
+	return dict_of_each_players_scores, x
 
-    for i in range(len(graph_usernames)):
-        y = [i for i in num_of_weeks[i]]
-        l1, = ax.plot(x[:len(y)], y, '--o', label=graph_usernames[i])
-        l1.set_color(custom_colours[i])
-    plt.ylim([0, 26])
-
-    ax.set_xlabel('Week No.')
-
-    plt.legend()
-    plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)
-    return b64encode(img.getvalue()).decode('utf8')
-
-    # return render_template('curUserStats.html', plot_url=plot_url, curr_user=curr_user, score_stats=s_stats)
 
 @app.route(f"/overall/stats")
-@cache.cached()
+# @cache.cached()
 def overall_stats():
-    # Data for the overall stats page
-    score_result = get_required_data()
-    usernames = list(score_result.keys())
-    custom_colours = ['#2631C3', '#AF33FF', '#F5B31E']
+	# Data for the overall stats page
+	score_result = get_required_data()
+	usernames = list(score_result.keys())
+	cust_colours = ["#2631C3", "#AF33FF", "#F5B31E"]
+	custom_colours_dict = {"Ben": "#2631C3", "Craig": "#AF33FF", "Hazel": "#F5B31E2"}
 
-    # Gets total scores for each user
-    graph_scores = list(sum(i['Scores']) for i in score_result.values())
+	average_of_averages = round(mean([score_result[usernames[i]]['Average'] for i in range(len(usernames))]), 2)
 
-    # graphs
-    pie_img = pie_chart(usernames, custom_colours, graph_scores)
-    barh_img = barh_chart(usernames, custom_colours, graph_scores)
-    barv_img = barv_chart(usernames, score_result, custom_colours)
-    line_img = overall_avgs_chart(usernames, score_result, custom_colours)
+	# Gets total scores for each user
+	graph_scores = list(sum(i['Scores']) for i in score_result.values())
 
-    return render_template('overallStats.html', pie_url=pie_img, bar_url=barh_img, avg_scores_url=barv_img, line_url=line_img)
+	# 0: Individual Average Scores with Names, 1: Just The Scores, 3: Average of Averages
+	avg_with_names, avg_scores, avg_of_avg = barv_chart(usernames, score_result)
 
+	line_img = overall_avgs_chart(usernames, score_result, iter(cust_colours))
+
+	line_graph_dict = {"score_dict": line_img[0], "x_axis" : line_img[1]}
+# pie_url=pie_img, bar_url=barh_img,
+# 	for i in range(len(usernames)):
+# 		usernames[i] += f" ({score_result[usernames[i]]['Average']})"
+	return render_template('overallStats.html', b_avg_with_names=avg_with_names, b_avg_scores=avg_scores,
+						   b_avg_of_avg=avg_of_avg, average_of_averages = average_of_averages, max=max(graph_scores),
+						   labels=usernames, values=graph_scores, line_graph_dict=line_graph_dict, colours=cust_colours,
+						   col_dict = custom_colours_dict)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
--- static/PubQuizJS.js
+++ static/PubQuizJS.js
@@ -10,3 +10,254 @@ $(".about-bar").click(function() {
 
 $("#regForm :input").prop("disabled", true);
 $("#regFormSubBut").prop("disabled", true);
+
+var ctx = document.getElementById("canvas");
+
+Chart.pluginService.register({
+    afterDraw: function(chart) {
+        if (typeof chart.config.options.lineAt != 'undefined') {
+        	var lineAt = chart.config.options.lineAt;
+            var ctxPlugin = chart.chart.ctx;
+            var xAxe = chart.scales[chart.config.options.scales.xAxes[0].id];
+            var yAxe = chart.scales[chart.config.options.scales.yAxes[0].id];
+
+            if(yAxe.min != 0) return;
+
+            ctxPlugin.strokeStyle = "white";
+        	ctxPlugin.beginPath();
+            lineAt = (lineAt - yAxe.min) * (100 / yAxe.max);
+            lineAt = (100 - lineAt) / 100 * (yAxe.height) + yAxe.top;
+            ctxPlugin.moveTo(xAxe.left, lineAt);
+            ctxPlugin.lineTo(xAxe.right, lineAt);
+            ctxPlugin.stroke();
+        }
+    }
+});
+
+var steps = 20;
+
+// Horizontal Bar Chart
+new Chart(document.getElementById("bar-chart-h"), {
+    type: 'horizontalBar',
+    data: {
+      labels: cust_labels,
+      datasets: [
+        {
+          backgroundColor: cust_colours,
+          data: cust_data
+        }
+      ]
+    },
+    options: {
+    indexAxis: 'y',
+      legend: { display: false },
+      title: {
+        display: true,
+        text: ''
+      },
+      scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+
+    animation: {
+            duration: 2000,
+            easing: 'easeOutBounce',
+        },
+
+      annotation: {
+      annotations: [{
+        type: 'line',
+        mode: 'horizontal',
+        scaleID: 'y-axis-0',
+        value: 100,
+        borderColor: 'rgb(255, 255, 255)',
+        borderWidth: 10,
+        label: {
+          enabled: true,
+          content: 'Test label'
+        }
+              }]
+    },
+
+      responsive: true,
+      maintainAspectRatio: true,
+      scaleOverride: true,
+      scaleSteps: steps,
+      scaleStepWidth: Math.ceil(max / steps),
+      scaleStartValue: 0,
+      scaleShowVerticalLines: true,
+      scaleShowGridLines : true,
+      barShowStroke : true,
+      scaleShowLabels: true
+    }
+});
+
+
+// Averages Bar Chart
+// Horizontal Bar Chart
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+      labels: b_avg_with_names,
+      datasets: [
+        {
+          backgroundColor: cust_colours,
+          data: b_avg_scores
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: ''
+      },
+      scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+
+    animation: {
+            duration: 2000,
+            easing: 'easeOutBounce',
+        },
+      // TODO: average of averages line
+      lineAt: b_avg_of_avg,
+//      annotation: {
+//      annotations: [{
+//        type: 'line',
+//        mode: 'horizontal',
+//        scaleID: 'y-axis-0',
+//        value: b_avg_of_avg,
+//        borderColor: 'rgb(255, 255, 255)',
+//        borderWidth: 10,
+//        label: {
+//          enabled: true,
+//          content: 'Test label'
+//        }
+//              }]
+//    },
+
+      responsive: true,
+      maintainAspectRatio: true,
+      scaleOverride: true,
+      scaleSteps: steps,
+      scaleStepWidth: Math.ceil(max / steps),
+      scaleStartValue: 0,
+      scaleShowVerticalLines: true,
+      scaleShowGridLines : true,
+      barShowStroke : true,
+      scaleShowLabels: true
+    }
+});
+
+//function line_graph(index, data)
+//{
+////var i = 0;
+//
+//    payload = {
+//        data: line_graph_dict['score_dict'][cust_labels[index]],
+//        label = cust_labels[i],
+//        backgroundColor = cust_colours[[cust_labels[index]],
+//        pointRadius = 2,
+//        borderColor = cust_colours[[cust_labels[index]]],
+//        fill = false
+//    }
+//    return payload;
+//}
+console.log(average_of_averages);
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: line_graph_dict['x_axis'],
+    datasets: [
+//    for(var i = 0; i < cust_labels.length; i++)
+//    {
+//        data: line_graph_dict['score_dict'][cust_labels[i]],
+//        label: [cust_labels[i],
+//        backgroundColor: cust_colours[[cust_labels[i]],
+//        pointRadius: 2,
+//        borderColor: cust_colours[[cust_labels[i]],
+//        fill: false
+//      },
+//    }
+//    }
+{
+        data: line_graph_dict['score_dict']['Ben'],
+        label: cust_labels[0],
+        backgroundColor: cust_colours[0],
+        pointRadius: 2,
+        borderColor: cust_colours[0],
+        fill: false
+      },
+        {
+        data: line_graph_dict['score_dict']['Craig'],
+        label: cust_labels[1],
+        backgroundColor: cust_colours[1],
+        pointRadius: 2,
+        borderColor: cust_colours[1],
+        fill: false
+      }, {
+        data: line_graph_dict['score_dict']['Hazelf8'],
+        label: cust_labels[2],
+        backgroundColor: cust_colours[2],
+        pointRadius: 2,
+        borderColor: cust_colours[2],
+        fill: false
+      }
+    ]
+  },
+  options: {
+   legend: {
+    display: false,
+  },
+
+  scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }],
+
+        xAxes: [{
+            scaleLabel:
+            {
+                display: true,
+                labelString: 'Week No.'
+            }
+        }]
+    },
+      tooltips: {
+    callbacks: {
+       title: function(tooltipItem, data) {
+        return 'Week: ' + this._data.labels[tooltipItem[0].index];
+    }
+    }
+  },
+    title: {
+      display: false,
+      text: ''
+    },
+
+    responsive: true,
+    maintainAspectRatio: true
+  }
+
+});
+
+new Chart(document.getElementById("pie-chart"), {
+    type: 'pie',
+    data: {
+      datasets: [{
+        backgroundColor: cust_colours,
+        data: cust_data
+      }]
+    }
+});
